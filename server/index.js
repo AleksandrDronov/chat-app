fb72da75d801a2d2c750e8745a9319b59d7ede1a
Aleksandr Dronov <dronovspb@gmail.com>
2023-05-07 17:24:54 +0300

    add auth

--- server/index.js
+++ server/index.js
@@ -5,7 +5,9 @@ import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
 import { Configuration, OpenAIApi } from "openai";
-import openAiRoutes from "./routes/openia.js"
+import openAiRoutes from "./routes/openia.js";
+import authRoutes from "./routes/auth.js"
+
 
 dotenv.config();
 
@@ -27,6 +29,7 @@ export const openai = new OpenAIApi(configuration);
 
 // ROUTES
 app.use("/openai", openAiRoutes);
+app.use("/auth", authRoutes);
 
 
 const PORT = process.env.PORT || 9000;
