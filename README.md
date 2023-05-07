062543da1c6a962fbc1af6890ef0b0ec6ba30de1
Aleksandr Dronov <dronovspb@gmail.com>
2023-05-07 20:39:07 +0300

    readme

--- README.md
+++ README.md
@@ -1,8 +1,22 @@
 #  Чат с ChatGPT ботом
 
-[ссылка](https://aleksandrdronov.github.io/chat-app/)
-
 ### Используемые технологии:
 1. ReactJS, RTK Query
 2. NodeJS, ExpressJS
 3. ChanEngine, OpenAi
+
+### Запуск проекта:
+
+1. Клонировать репозиторий.
+
+   git clone git@github.com:AleksandrDronov/chat-app.git
+
+2. Войти в папку фронтенда, установить зависимости, запустить.
+
+   cd chat-app/client && npm i && npm run dev
+
+3. Открыть новое окно терминала
+
+3. Войти в папку бекенда, установить зависимости, запустить.
+
+   cd server && npm i && npm run dev
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,7 +5,7 @@ import { Provider } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
 import { api } from "./state/api.js";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 export const store = configureStore({
   reducer: {
@@ -18,8 +18,8 @@ setupListeners(store.dispatch);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </Provider>
 );
