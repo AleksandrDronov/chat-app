a6021bbf8fdad8601c07bbf32f64974d6860182a
Aleksandr Dronov <dronovspb@gmail.com>
2023-05-07 18:29:06 +0300

    deploy

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,8 +4,12 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/app-chat/",
   plugins: [react()],
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src")}]
+  },
+  server: {
+    open: true
   }
 });
